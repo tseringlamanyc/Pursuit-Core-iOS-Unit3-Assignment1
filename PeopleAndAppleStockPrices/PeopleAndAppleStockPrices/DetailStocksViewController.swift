692ce8623917f314141f160a2cb9aaebcb033afd
Tsering Lama <tseringlama@pursuit.org>
2019-12-03 14:08:54 -0500

    got the view controller set up

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/DetailStocksViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailStocksViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Tsering Lama on 12/3/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class DetailStocksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Supporting Files/json/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Tsering Lama on 12/3/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
